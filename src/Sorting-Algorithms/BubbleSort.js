8fd1e70e8966c8b5ed69faea697110f2ac8e989c
Shaarang Singh <shaarangg.singh@gmail.com>
2021-11-22 18:57:01 +0530

    feat: Animation

--- src/Sorting-Algorithms/BubbleSort.js
+++ src/Sorting-Algorithms/BubbleSort.js
@@ -1,4 +1,18 @@
-function BubbleSort() {
-	console.log("BubbleSort");
+function BubbleSort(arr) {
+	const l = arr.length;
+	const newArr = arr.slice();
+	const arrSteps = [];
+	for (let i = 0; i < l - 1; i++) {
+		for (let j = 0; j < l - i - 1; j++) {
+			if (newArr[j] > newArr[j + 1]) {
+				let temp = newArr[j];
+				newArr[j] = newArr[j + 1];
+				newArr[j + 1] = temp;
+			}
+			arrSteps.push(newArr.slice());
+		}
+	}
+	// console.log(arrSteps);
+	return arrSteps;
 }
 export default BubbleSort;
