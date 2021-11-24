0ee44467a8abc59112d17a85392fc10ef112f6e6
Shaarang Singh <shaarangg.singh@gmail.com>
2021-11-24 21:58:46 +0530

    feaT: Merge Sort Animatio

--- src/Sorting-Algorithms/MergeSort.js
+++ src/Sorting-Algorithms/MergeSort.js
@@ -1,35 +1,77 @@
-function merge(firstHalf, secondHalf) {
-	const sortedArray = [];
+function merge(newArr, left, mid, right, newColor, arrSteps, colorSteps) {
+	const n1 = mid - left + 1;
+	const n2 = right - mid;
+	const L = new Array(n1);
+	const R = new Array(n2);
+	for (let i = 0; i < n1; i++) {
+		L[i] = newArr[left + i];
+		newColor[left + i] = 1;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+	}
+	for (let i = 0; i < n2; i++) {
+		R[i] = newArr[mid + i + 1];
+		newColor[mid + i + 1] = 1;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+	}
 	let i = 0,
-		j = 0;
-	while (i < firstHalf.length && j < secondHalf.length) {
-		if (firstHalf[i] < secondHalf[j]) {
-			sortedArray.push(firstHalf[i++]);
+		j = 0,
+		k = left;
+	while (i < n1 && j < n2) {
+		if (L[i] < R[j]) {
+			newArr[k] = L[i];
+			i++;
 		} else {
-			sortedArray.push(secondHalf[j++]);
+			newArr[k] = R[j];
+			j++;
 		}
+		newColor[k] = 2;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+		newColor[k] = 0;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+		k++;
 	}
-	while (i < firstHalf.length) sortedArray.push(firstHalf[i++]);
-	while (j < secondHalf.length) sortedArray.push(secondHalf[j++]);
-	return sortedArray;
+	while (i < n1) {
+		newArr[k] = L[i];
+		newColor[k] = 2;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+		newColor[k] = 0;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+		i++;
+		k++;
+	}
+	while (j < n2) {
+		newArr[k] = R[j];
+		newColor[k] = 2;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+		newColor[k] = 0;
+		colorSteps.push(newColor.slice());
+		arrSteps.push(newArr.slice());
+		j++;
+		k++;
+	}
 }
-function mergeSortHelper(newArr) {
-	const l = newArr.length;
-	if (l === 1) {
-		return newArr;
-	}
-	const m = Math.floor(l / 2);
-	const firstHalf = mergeSortHelper(newArr.slice(0, m));
-	const secondHalf = mergeSortHelper(newArr.slice(m, l));
-	const sortedArray = merge(firstHalf, secondHalf);
-	return sortedArray;
+function mergeSortHelper(newArr, left, right, newColor, arrSteps, colorSteps) {
+	if (left >= right) return;
+	const mid = Math.floor((left + right) / 2);
+	mergeSortHelper(newArr, left, mid, newColor, arrSteps, colorSteps);
+	mergeSortHelper(newArr, mid + 1, right, newColor, arrSteps, colorSteps);
+	merge(newArr, left, mid, right, newColor, arrSteps, colorSteps);
 }
 function MergeSort(arr, color) {
 	const newColor = color.slice();
+	const newArr = arr.slice();
 	const arrSteps = [];
 	const colorSteps = [];
-	const newArr = mergeSortHelper(arr.slice());
-	console.log(newArr);
+	mergeSortHelper(newArr, 0, newArr.length - 1, newColor, arrSteps, colorSteps);
+	colorSteps[colorSteps.length - 1] = new Array(newArr.length).fill(3);
+	// console.log(newArr);
 	return { arrSteps, colorSteps };
 }
 export default MergeSort;
